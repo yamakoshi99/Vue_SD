759b7403694c46b19edfdaf3bcea4cd18d3d0f9d
yamakoshi <takuyageemzuki@gmai.com>
2022-11-14 00:15:36 +0900

    SD_morning(朝の自己啓発)　実装中

--- src/components/SD_morning.vue
+++ src/components/SD_morning.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="SD"></div>
-    <h1>{{ SD }}</h1>
+  <div>
+    <h1>自己啓発管理</h1>
+  </div>
 </template>
-<script>
 
+<script>
+export default {
+  data () {
+    return {
+      name: 'SD_morning'
+    }
+  }
+}
 </script>
+
+<style>
+
+</style>
